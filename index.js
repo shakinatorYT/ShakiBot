fcdda13f1ba771d1e1adebeff7bf28afaf656e99
shakinatorYT <38700912+shakinatorYT@users.noreply.github.com>
2019-03-23 15:47:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const bot = new Discord.Client();
 const cfg = require('./index.json'); // a garder en version desktop
 */
 const token = process.env.token // a garder en version heroku
-const prefix = ("/");
+const prefix = ("?");
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
-    bot.user.setActivity('rien').catch(console.error)
+    bot.user.setActivity('Surveille').catch(console.error)
 });
 bot.login(token)
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
+        return channel.send('Bienvenue sur le serveur de la team MPX' + member.displayName)
         console.log(`${member.displayName} à rejoind le serveur.`)
     }).catch(console.error)
 });
@@ -28,8 +28,8 @@ bot.on('message', msg => {
             msg.reply('Je suis d\'accord avec toi.')
     }
     if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
+        msg.channel.send("https://www.youtube.com/channel/UCOhGDL7a89EkC91H6Kp-Alg?view_as=subscriber")
+        console.log("Une personne a demandé pour aller sur ta chaine.")
     }
 
 });
